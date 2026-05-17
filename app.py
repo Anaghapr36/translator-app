782a790559cb9171582437ed56de75e6e28217b9
Anagha P R <anaghaprbharadwaj@gmail.com>
2026-05-17 16:27:52 +0530

    Added translation functionality

--- app.py
+++ app.py
@@ -1,10 +1,21 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+from googletrans import Translator
 
 app = Flask(__name__)
+translator = Translator()
 
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def home():
-    return render_template('index.html')
+    translated_text = ""
+
+    if request.method == 'POST':
+        text = request.form['text']
+        lang = request.form['lang']
+
+        result = translator.translate(text, dest=lang)
+        translated_text = result.text
+
+    return render_template('index.html', translated_text=translated_text)
 
 if __name__ == "__main__":
     app.run(debug=True)
